2a64c5e3f1eb8a29b73a23736733f771d6a98d90
Andres Vega <handresvegarodriguez@gmail.com>
2022-05-18 13:49:35 -0400

    -- Replace header AMP

---
+++ replace-header-amp.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Plugin Name:       Replace Header AMP
+ * Plugin URI:        https://andrevega.com/
+ * Description:       This plugin helps to add a custom Header with menu for the AMP Legacy theme in the template mode Reader for AMP plugin WordPress
+ * Version:           0.1
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Andrés Vega
+ * Author URI:        https://andrevega.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://andrevega.com/
+ * Text Domain:       replace-header-amp
+ * Domain Path:       /languages
+ */
